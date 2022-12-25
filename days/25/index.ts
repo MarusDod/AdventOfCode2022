c081a6f22daa8f4fec69278adf342887891f5b7a
MarusDod <markitos.2001@hotmail.com>
2022-12-25 17:21:42 +0000

    day 25 - alternative solution

--- days/25/index.ts
+++ days/25/index.ts
@@ -3,28 +3,37 @@ import Problem from "../../lib/problem"
 
 type SnafuDigit = 0 | 1 | 2 | '-' | '='
 
-type Multiplier = (y: number) => number
-
-const toDecimal = (num: SnafuDigit[]): number =>
-    fold(
-        zip(
-            num,
-            Array.from(iterate(x => x*5,1/5,num.length)),
-        ),
-        0,
-        (prev,[digit,place]) => prev + (digit === '-' ? -place : digit === '=' ? -(2*place) : digit*place)
-    )
-
 const snafuDigits: SnafuDigit[] = [0,1,2,'=','-']
 
-const toSnafu = (num: number): SnafuDigit[] => {
-    if(num === 0)
-        return []
+const snafuValues = zip([0,1,2,-2,-1],snafuDigits)
 
-    const rem = mod(num,5)
-    const div = Math.floor(num / 5) + (rem > 2 ? 1 : 0)
+const sumSnafuDigit = (a: SnafuDigit,b: SnafuDigit,carry: number): [SnafuDigit,number] => {
+    const sum = snafuValues.find(q => q[1] === a)![0] + snafuValues.find(q => q[1] === b)![0] + carry
+    const actualSum = mod(sum,5)
 
-    return [...toSnafu(div),snafuDigits[rem]]
+    return [snafuDigits[actualSum],sum < -2 ? -1 : sum > 2 ? 1 : 0]
+}
+
+const addSnafus = (a: SnafuDigit[],b: SnafuDigit[]): SnafuDigit[] => {
+    let res: SnafuDigit[] = []
+    let carry = 0
+    const maxLen = Math.max(a.length,b.length)
+
+    a = a.reverse()
+    b = b.reverse()
+
+    for(let i of range(maxLen,0)){
+        const [sum,newCarry] = sumSnafuDigit(a[i] ?? 0,b[i] ?? 0,carry)
+
+        carry = newCarry
+
+        if(i === maxLen && sum === 0)
+            break;
+
+        res.push(sum)
+    }
+
+    return res.reverse()
 }
 
 const solution: Problem<SnafuDigit[][],string> = {
@@ -32,11 +41,11 @@ const solution: Problem<SnafuDigit[][],string> = {
         return rawInput
             .split('\n')
             .slice(0,-1)
-            .map(x => x.split('').reverse()) as SnafuDigit[][]
+            .map(x => x.split('').map(x => isNaN(parseInt(x)) ? x : parseInt(x))) as SnafuDigit[][]
     },
 
     solve1(nums){
-        return toSnafu(sum(nums.map(toDecimal))).join('')
+        return nums.reduce((prev,cur) => addSnafus(prev,cur)).join('')
     },
 
     solve2(num){
